ef56a5d13c6e532973f562151199c7e4f25203d1
Bhushan Rajput <bhushanrajput2298@gmail.com>
2020-07-17 19:37:16 +0530

    Updates

---
+++ precache-manifest.8c32dfd4252c2782fc29a3650883a715.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcda6fa8236e19497c8fc7347f2aa25",
+    "url": "/mars-rover/index.html"
+  },
+  {
+    "revision": "bf072eab7c51c4c56422",
+    "url": "/mars-rover/static/css/main.dbf79c10.chunk.css"
+  },
+  {
+    "revision": "bc07ecd6586a3ea775b9",
+    "url": "/mars-rover/static/js/2.c5ee0e87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mars-rover/static/js/2.c5ee0e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf072eab7c51c4c56422",
+    "url": "/mars-rover/static/js/main.5a6e618b.chunk.js"
+  },
+  {
+    "revision": "aaef881be8b0f7b0b696",
+    "url": "/mars-rover/static/js/runtime-main.cdac958e.js"
+  },
+  {
+    "revision": "ae01d38c41f38dee4dc4f65fe3c77f63",
+    "url": "/mars-rover/static/media/mars-rover2.ae01d38c.jpg"
+  }
+]);
